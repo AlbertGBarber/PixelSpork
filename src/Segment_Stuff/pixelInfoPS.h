46f6ab815aed8d9e110e9fd45b1d80ddf9e93aee
Albert Barber <albert.barber1@gmail.com>
2025-05-25 06:54:47 -0700

    Naming bug fix pt2

--- src/Segment_Stuff/pixelInfoPS.h
+++ src/Segment_Stuff/pixelInfoPS.h
@@ -1,7 +1,6 @@
 #ifndef pixelInfoPS_h
 #define pixelInfoPS_h
 
-//
 //struct used in segDrawUtils::getPixelColor
 //used to store all the variables associated with a segment pixel for setting it's color
 struct pixelInfoPS {
